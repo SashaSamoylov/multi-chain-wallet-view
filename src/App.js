a42c95930561e548c27da38c96cd36ca35634638
SashaSamoylov <dragonboy950501@outlook.com>
2022-01-15 16:16:45 -0700

    integrate basic moralis components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-function App() {
+import React from 'react'
+import { useMoralis } from 'react-moralis'
+
+export default function App() {
+	const { authenticate, isAuthenticated, user } = useMoralis()
+
+	if (!isAuthenticated) {
+		return (
+			<div>
+				<button onClick={() => authenticate()}>Authenticate</button>
+			</div>
+		)
+	}
+
 	return (
-		<div className='text-2xl bg-gray-700 py-4 px-2 text-white'>
-			Tailwind is installed
+		<div>
+			<h1>Welcome {user.get('username')}</h1>
 		</div>
 	)
 }
-
-export default App
